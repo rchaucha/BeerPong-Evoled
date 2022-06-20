945f6372c6c6e3224889f18e40574cc7085df536
Rémi Chauchadis <remi.chaucha@gmail.com>
2022-06-20 10:26:03 +0200

    Attempt to have the ProjectorView as an independent window even though it has a parent, to be tested

--- BeerPong-Ultimate/src/main/BPUApp.cpp
+++ BeerPong-Ultimate/src/main/BPUApp.cpp
@@ -89,6 +89,7 @@ int BPUApp::init()
    _main_gui.setScreen(qApp->screens()[0]);
 
    _projector_win.setParent(&_main_gui);
+   _projector_win.setWindowFlags(Qt::Window);
    _projector_win.show();
 
    _projector_win.setScreen(qApp->screens()[1]);
